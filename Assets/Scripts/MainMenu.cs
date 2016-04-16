4d79786e8d3602cad069fc247a4c2d4122e3e288
Todor Nikolov <todor.nikolov@mail.bg>
2016-04-16 16:11:54 +0300

    main menu - start to second scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+//using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class MainMenu : MonoBehaviour {
+
+    public void RunTour()
+    {
+        Application.LoadLevel("PlaneScene");
+    }
+
+    public void ExitProgram (){ 
+        Application.Quit();
+	}
+}
