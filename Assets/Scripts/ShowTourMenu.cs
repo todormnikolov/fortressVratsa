8ad7a3b59101749e47fc960b9522f1944388d09b
Todor Nikolov <todor.nikolov@mail.bg>
2016-04-17 15:53:54 +0300

    final

---
+++ Assets/Scripts/ShowTourMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowTourMenu : MonoBehaviour {
+
+    public GameObject tourMenu;
+    public bool isShowing;
+
+    void Start()
+    {
+        tourMenu.SetActive(false);
+    }
+
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            tourMenu.SetActive(isShowing);
+        }
+	}
+}
---
+++ Assets/Scripts/TourMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TourMenu : MonoBehaviour {
+
+    public GameObject tourMenu;
+
+    public void PauseTour()
+    {
+        tourMenu.SetActive(false);
+    }
+
+    public void EndTour()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void ExitProgram()
+    {
+        Application.Quit();
+    }
+}
